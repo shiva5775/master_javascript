d330b998cb109e9d8715a3526627ba8be83160a2
shiva gupta <shivbagupta@gmail.com>
2024-05-26 13:33:04 +0530

    loop in array

--- 00_array/01_script.js
+++ 00_array/01_script.js
@@ -1,3 +1,7 @@
 let fruits = ["mango", "apple", "banana", "orange", "grapes", "watermelon"];
 console.log(fruits);
 console.log(fruits.length);
+
+for (const fruit of fruits) {
+    console.log(fruit);
+}
