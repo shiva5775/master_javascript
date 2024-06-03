d51c7911dc73f53eadd3b7270828070248ff9586
shiva5775 <140914692+shiva5775@users.noreply.github.com>
2024-06-03 11:16:57 +0000

    01_object file

--- 00_array/01_script.js
+++ 00_array/01_script.js
@@ -1,2 +1,5 @@
 let fruits = ["mango","apple","banana","orange","grapes","watermelon"];
 console.log(fruits);
+
+let neeFruits=fruits.join(' ');
+console.log(neeFruits);
---
+++ 01_object_dive/01_object.js
@@ -0,0 +1,15 @@
+// function getInfo(person,year){
+//  person.name='Gupta';
+//   year=1998;
+//   console.log(person,year);
+// }
+
+// const bio={name:'Shiva',age:25};
+// const birthYear=2000;
+
+// getInfo(bio,birthYear);
+// console.log(bio,birthYear);
+
+
+console.log(num);
+const num = 10;
