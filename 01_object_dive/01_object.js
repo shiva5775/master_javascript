cbcc98d3f43d62efb4769a4e1ca7b8a599f6038c
shiva5775 <140914692+shiva5775@users.noreply.github.com>
2024-06-09 09:58:24 +0000

    add learn date

--- 01_object_dive/01_object.js
+++ 01_object_dive/01_object.js
@@ -10,6 +10,15 @@
 // getInfo(bio,birthYear);
 // console.log(bio,birthYear);
 
-
-console.log(num);
+// console.log(num);
 const num = 10;
+
+let num1 = new Number(200);
+console.log(num1);
+
+let min = 12;
+let max = 17;
+let rangeNumber = Math.floor(Math.random() * (max - min + 1) + min);
+console.log(rangeNumber);
+
+
